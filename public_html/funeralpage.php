7db7703937d0c2f96815ad3fc4a29934e320654c
EJlearner <rzrshrp@gmail.com>
2018-01-27 15:36:13 -0500

    gaskins page changes and styling

--- public_html/funeralpage.php
+++ public_html/funeralpage.php
@@ -32,139 +32,64 @@ Baltimore, MD 21229<br>
 3457 Flannery Lane<br>
 Gwynn Oak, MD 21207</p>
 ";
-	break;
-	case("dsutherland");
-	$name="Delores Sutherland";
-	$content=
-"<h1>Homegoing Service for Delores Sutherland</h1>
-<br>
-<h3>Family Viewing</h3>
-<a href=\"http://www.marchfh.com/\">March Funeral Home - East</a><br>
-1101 East North Avenue, Baltimore, MD 21202<br>
-Wednesday, June 2, 2010 5pm-7pm<br>
-<iframe width=\"300\" height=\"300\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=1101+East+North+Avenue,+Baltimore,+MD+21202-5899&amp;sll=39.311664,-76.603332&amp;sspn=0.006998,0.013078&amp;gl=us&amp;ie=UTF8&amp;hq=&amp;hnear=1101+E+North+Ave,+Baltimore,+Maryland+21202&amp;ll=39.311648,-76.603332&amp;spn=0.001749,0.00327&amp;t=h&amp;z=17&amp;output=embed\"></iframe><br />
-<h3>Homegoing Services</h3>
-<p><a href=\"http://www.thecitytemple.org\">Baltimore City Temple (Baptist)</a><br>
-317 Dolphin Street, Baltimore, MD 21229<br>
-<p>Thursday, June 3, 2010<br>
-Wake: 10:00am - 11:00am<br>
-Funeral:11:00am</p>
-<h3>Cards Can Be Sent to</h3>
-<p>Christine C Barksdale<br>
-1522 Edison Highway<br>
-Baltimore, MD 21213<br>
-</p>";
-	break;
-	
-	case("bettyreed");
-	$name="Betty Reed";
-	$content=
-"<h1>Homegoing Service for Betty Reed</h1>
-Mother of Gregory Reed, daughter of Dora Hardee, sister of Dale Knight
-<br>
-<h3>Viewing</h3>
-<a href=\"http://www.wyliefh.com/\">Wylies Funeral Home</a><br>
-9200 Liberty Road, Baltimore, MD 21215<br>
-Sunday, February 21, 2010 1pm-4pm<br>
-<iframe width=\"300\" height=\"300\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://maps.google.com/maps?hl=en&amp;client=firefox-a&amp;q=wylie's+funeral+home&amp;ie=UTF8&amp;hq=wylie's+funeral+home&amp;hnear=Gwynn+Oak,+MD&amp;cid=3399539689253795038&amp;ll=39.371995,-76.802287&amp;spn=0.009953,0.012875&amp;z=15&amp;iwloc=A&amp;output=embed\"></iframe><br />
-<h3>Homegoing Services</h3>
-<p><a href=\"http://www.thecitytemple.org\">Baltimore City Temple (Baptist)</a><br>
-317 Dolphin Street, Baltimore, MD 21229<br>
-Monday, February 22, 2010<br>
-Family Hour: 10am - 10:30am<br>
-Funeral to Follow Family Hour</p>
-<h3>Cards Can Be Sent to</h3>
-<p>Gregory Reed<br>
-2903 Haverford Road<br>
-Baltimore, MD 21216<br>
-</p>
-";
-	break;
-	
-	case("johnnyhickman");
-	$name="Johnny Hickman";
-	$content=
-"<h1>Homegoing Service for Johnny Hickman</h1>
-<!--Mother of Gregory Reed, daughter of Dora Hardee, sister of Dale Knight-->
-<br>
-<h3>Viewing</h3>
-The James A. Morton Funeral Home<br>
-1701 Laurens St, Baltimore, MD 21217<br>
-Thursday, March 10, 2011 3pm-7pm<br>
-<iframe width=\"300\" height=\"300\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=1701+laurens+street&amp;aq=&amp;sll=39.347043,-76.543121&amp;sspn=0.49117,0.837021&amp;ie=UTF8&amp;hq=&amp;hnear=1701+Laurens+St,+Baltimore,+Maryland+21217&amp;ll=39.303022,-76.645088&amp;spn=0.019925,0.025749&amp;z=14&amp;iwloc=A&amp;output=embed\"></iframe><br />
-<h3>Homegoing Services</h3>
-<p><a href=\"http://www.thecitytemple.org\">Baltimore City Temple (Baptist)</a><br>
-317 Dolphin Street, Baltimore, MD 21229<br>
-Friday, March 11, 2011<br>
-Wake: 10am - 10:30am<br>
-Funeral to Follow Wake</p>
-<!--<h3>Cards Can Be Sent to</h3>
-<p>Gregory Reed<br>
-2903 Haverford Road<br>
-Baltimore, MD 21216<br>-->
-</p>
-";
-	break;
-	case("indapulliam");
-	$name="Inda Pulliam";
-	$content=
-"<h1>Homegoing Service for Inda Pulliam</h1>
-Mother of Pat Johnson
-<br>
-<h3>Viewing</h3>
-<a href=\"http://www.marchfh.com/\">March Funeral Home - West</a><br>
-4300 Wabash Avenue, Baltimore, MD 21215<br>
-Monday, January 2, 2012 8:30 a.m.-8 p.m.<br>
-<h3>Homegoing Services</h3>
-<p><a href=\"http://www.thecitytemple.org\">Baltimore City Temple (Baptist)</a><br>
-317 Dolphin Street, Baltimore, MD 21229<br>
-Tuesday, January 3, 2012<br>
-Wake: 11:00 a.m.<br>
-Service: 11:30 a.m.</p>
 
-<h3>Internment</h3>
-Garrison Forest Veteran's Cemetery - 2:30 p.m.<br>
-";
-	break;
-	case("irenewoodland");
-	$name="Irene Woodland";
-	$content=
-		"<h1>Homegoing Service for Irene Woodland</h1>
-		<h3>Viewing</h3>
-		<a href=\"http://www.marchfh.com/\">March Funeral Home - West</a><br>
-		4300 Wabash Avenue, Baltimore, MD 21215<br>
-		Tuesday, January 3, 2012 
-		Family Viewing: 12:30 a.m.<br>
-		Public Viewing Afterward
-		<h3>Homegoing Services</h3>
-		<p><a href=\"http://www.thecitytemple.org\">Baltimore City Temple (Baptist)</a><br>
-		317 Dolphin Street, Baltimore, MD 21229<br>
-		Wednesday, January 4, 2012<br>
-		Wake: 10:30 a.m.<br>
-		Service: 11:00 a.m.</p>
+case("cgaskins");
+//defaults
+$image = "funeralcross.jpg";
+$imageAlt = "Christian Cross With Clouds";
 
-		<h3>Internment</h3>
-		Garrison Forest Veteran's Cemetery<br>
+$name="Minister Charles Gaskins";
+$serviceSiteWebsite = "http://www.thecitytemple.org";
+$serviceSiteName = "Baltimore City Temple (Baptist)";
+$serviceSiteAddress = "317 Dolphin Street, Baltimore, MD 21229";
+$serviceDate = "Saturday, February 3";
+$serviceTimeAndInfo =
+	"Wake: 10:00 a.m. - 11:00 a.m.<br>
+	Funeral: 11:00 a.m.</p>";
+$additionalContent ="
+<h3 class='ul-header'>Donations May Be Made To</h3>
+<ul>
+	<li>John’s Hopkins Cardiovascular Center</li>
+	<li>John’s Hopkins Hospital</li>
+</ul>
+	";
+
+$content=
+"<h1>Memorial Service for $name</h1>
+<hr>
+<div class='information'>
+<h3>Memorial Service</h3>
+<p><a href='$serviceSiteWebsite'>$serviceSiteName</a><br>
+$serviceSiteAddress<br>
+$serviceDate<br>
+$serviceTimeAndInfo</p>
+$additionalContent
+</div>
+<figure>
+	<img src='images/gaskinbio.jpg' alt='Minister Charles Gaskins Picture'>
+</figure>
 ";
-	break;
-	case("ddavenport");
+
+break;
+
+case("ddavenport");
 		//defaults
 		$image = "funeralcross.jpg";
 		$imageAlt = "Christian Cross With Clouds";
-		
+
 		$name="Delores Davenport";
 		$viewingSiteWebsite = "http://www.mortonfuneralhomes.com/";
 		$viewingSiteName = "James A. Morton Funeral Home";
 		$viewingSiteAddress = "1701 Laurens St, Baltimore, MD 21217";
 		$viewingDate = "Tuesday, March 29";
-		$viewingTimeAndInfo = 
+		$viewingTimeAndInfo =
 			"Family Viewing: 2:30 - 3:00 p.m.<br>
 			Public Viewing: 3:00 - 7:00 p.m.";
 		$serviceSiteWebsite = "http://www.thecitytemple.org";
 		$serviceSiteName = "Baltimore City Temple (Baptist)";
 		$serviceSiteAddress = "317 Dolphin Street, Baltimore, MD 21229";
 		$serviceDate = "Wednesday, March 30";
-		$serviceTimeAndInfo = 
+		$serviceTimeAndInfo =
 			"Wake: 11:00 a.m. - 11:30 a.m.<br>
 			Funeral: 11:30 a.m.</p>";
 		$internMentSite = "Garrison Forest Veteran's Cemetery";
@@ -172,7 +97,7 @@ Garrison Forest Veteran's Cemetery - 2:30 p.m.<br>
 		$additionalContent =
 			"<h3>Additional Information</h3>
 			<p>Repast will follow internment</p>
-			<p>Miss Delores was a dedicated member of the Temple Choir for many years. Please keep Deacon Davenport and the rest of the family in your prayers. 
+			<p>Miss Delores was a dedicated member of the Temple Choir for many years. Please keep Deacon Davenport and the rest of the family in your prayers.
 We are asking choir members to bring desserts, a favorite dish, water or punch.<br>Thank you and be blessed.</p>
 			";
 
@@ -200,16 +125,16 @@ We are asking choir members to bring desserts, a favorite dish, water or punch.<
 }
 
 ?>
-<html> 
-<head> 
+<html>
+<head>
 <?php include "stylelogic.php" ?>
 
-<meta name="keywords" content="baltimore, baptist, city, temple" > 
-<meta name="description" content="City Temple Baptist Church - <?php echo $name ?> funeral service" > 
-<meta name="revised" content="Earl Jones, <?php echo date ("F d Y H:i:s.", filemtime(__FILE__))?>" > 
+<meta name="keywords" content="baltimore, baptist, city, temple" >
+<meta name="description" content="City Temple Baptist Church - <?php echo $name ?> funeral service" >
+<meta name="revised" content="Earl Jones, <?php echo date ("F d Y H:i:s.", filemtime(__FILE__))?>" >
 <meta http-equiv="content-type" content="text/html; charset=utf-8" >
-<meta name="generator" content="Notepad++" > 
-<title>City Temple Baptist Church - Funeral Service for <?php echo $name ?></title> 
+<meta name="generator" content="Notepad++" >
+<title>City Temple Baptist Church - Funeral Service for <?php echo $name ?></title>
 </head>
 
 <body>
