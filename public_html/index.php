f08ffa473ca1d13a5e9381e2e88e7c23919198d8
EJLearner <rzrshrp@gmail.com>
2018-05-29 20:04:57 -0400

    add and use pdf

--- public_html/index.php
+++ public_html/index.php
@@ -140,9 +140,7 @@
 				addevent("March 30 11:00 a.m.","<a href='funeralpage.php?p=ddavenport'>Delores Davenport Funeral Service</a>","2016-03-31");
 				addevent(
 					"July 21 1:00 p.m.",
-					"<a href='images/temp/2018-organscape.jpg' onclick=\"openOnClick(event, 'name', .75)\">
-						Artscape to Organscape 2018
-					</a>",
+					"<a href='images/temp/2018-organscape.pdf'>Artscape to Organscape 2018</a>",
 					"2018-07-22"
 				);
 				addevent(
